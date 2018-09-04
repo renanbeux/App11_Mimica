ed9a7858ad7ed81e5b1ee2def94a7f83ae87c3b0
Renan Beux <nanerxueb@hotmail.com>
2018-09-04 09:52:43 -0300

    Adicionar arquivos de projeto.

---
+++ App11_Mimica/App11_Mimica/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App11_Mimica
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
